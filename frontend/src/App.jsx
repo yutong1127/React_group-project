4ac9fdc9368d2594522d0349e563b3dadea23a6f
JingyiEureka <40253730+JingyiEureka@users.noreply.github.com>
2023-03-27 20:13:22 +1300

    main branch setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-import ButtonAppBar from './TestCmpnts/NavBarTest';
-import BarChartTest from './TestCmpnts/BarChartTest';
-import BasicButtons from './TestCmpnts/ButtonTest';
+
 
 function App() {
-  
+
 
   return (
     <div className="App">
-    <BasicButtons />
-      <ButtonAppBar />
-      <div className='Barchart'>
-        <BarChartTest/>
-      </div>
+      <p>testing</p>
     </div>
   )
 }
