817e15051b79fc1ae7e51ae246cf14961801a214
JingyiEureka <40253730+JingyiEureka@users.noreply.github.com>
2023-04-14 16:34:37 +1200

    Update main.jsx
    updated quotation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 import { AppContextProvider } from './AppContextProvider'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppContextProvider>
       <App />
