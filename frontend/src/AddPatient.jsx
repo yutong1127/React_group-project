3eb7731942621724141041f2bf607e6db4a4277d
chanjant <chanjant@gmail.com>
2023-04-14 14:49:34 +1200

    Update AddPatient.jsx

--- frontend/src/AddPatient.jsx
+++ frontend/src/AddPatient.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { FormControl, FormGroup, FormLabel,FormControlLabel,Checkbox,Radio, RadioGroup, Switch } from '@mui/material';
+import { FormControl, FormGroup, FormLabel,FormControlLabel,Checkbox,Radio, RadioGroup, Switch, Button } from '@mui/material';
 
 
 const locations = [
@@ -24,11 +24,29 @@ const locations = [
   ];
 
 export default function AddPatient(){
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = {
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: formData.get('location'),
+      responsibleClinicians: {
+        mrX: formData.get('mrX'),
+        mrY: formData.get('mrY'),
+      }, 
+      quickAdd: formData.get('quickAdd'),
+      notification: formData.get('notification'),
+    };
+    console.log(data);
+  }
     return(
         <Box sx={{'& .MuiTextField-root': { m: 1}}}>
-          <TextField multiline fullWidth label="Name"/>
-          <TextField multiline fullWidth label="Description" rows={4} />
-          <TextField select fullWidth  label="Location" defaultValue="Auckland"  SelectProps={{
+          <form onSubmit={handleSubmit}>
+          <TextField multiline fullWidth label="Name" name="name"/>
+          <TextField multiline fullWidth label="Description" rows={4} name="description"/>
+          <TextField select fullWidth  label="Location" defaultValue="Auckland"  name="location" SelectProps={{
             native: true,
           }}>
             {locations.map((option) => (
@@ -40,21 +58,23 @@ export default function AddPatient(){
           <FormControl sx={{ m: 3 }} component="fieldset" variant="standard" style={{ display: 'flex', justifyContent: 'flex-start' }}>
             <FormLabel style={{textAlign: "left"}} component="label">Responsible Clinician</FormLabel>
             <FormGroup >
-                <FormControlLabel control={<Checkbox/>} label="Mr X" />
-                <FormControlLabel control={<Checkbox/>} label="Mr Y" />
+                <FormControlLabel control={<Checkbox/>} label="Mr X" name="mrX"/>
+                <FormControlLabel control={<Checkbox/>} label="Mr Y" name="mrY" />
             </FormGroup>
           </FormControl>
           <FormControl sx={{ m: 3 }} component="fieldset" variant="standard" style={{ display: 'flex', justifyContent: 'flex-start' }}>
           <FormLabel style={{textAlign: "left"}} component="label">Quick add</FormLabel>
             <RadioGroup>
-            <FormControlLabel value="blood-test" control={<Radio />} label="Blood Test" />
-            <FormControlLabel value="radiology" control={<Radio />} label="Radiology" />
+            <FormControlLabel value="blood-test" control={<Radio />} label="Blood Test" name="quickAdd"/>
+            <FormControlLabel value="radiology" control={<Radio />} label="Radiology" name="quickAdd"/>
             </RadioGroup>
           </FormControl>
           <FormGroup>
-            <FormControlLabel control={<Switch defaultChecked />} label="Notification" />
+            <FormControlLabel control={<Switch defaultChecked />} label="Notification" name="notification" />
          </FormGroup>
-        </Box>
+         <Button style={{display: 'flex'}} variant="contained" type="submit">Add</Button>
+         </form>
+      </Box>
     )
     
 }
