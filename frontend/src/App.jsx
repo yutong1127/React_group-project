5f66b532f06848c54d441c475445a4ef0e355546
jiewenli95 <jli429@aucklanduni.ac.nz>
2023-04-14 18:48:21 +1200

    PatientDetails Reorganized

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,35 @@
 import { useState } from "react";
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./LoginPage/LoginPage";
-import React from "react";
-import Patient_detail from "./PatientDetails/Patient_detail";
-// import { Routes, Route, Navigate } from "react-router-dom";
+// import Patient_detail from "./PatientDetails/Patient_detail";
 import MyProfile from './MyProfilePage/MyProfile';
 import MyTeam from './MyTeamPage/MyTeam';
-
 import BasicTabs from './AddModifyPatient/PatientInfoTab';
-import Notifications from './notifications/Notification'
-
+import Notifications from './notifications/Notification';
+import PageWithNavbar from "./PageWithNavBar";
+import PatientListGrid from "./PatientDetails/PatientListGrid";
 
 
 function App() {
   return (
-    <div className="App">
-      {/* <LoginPage /> */}
-      {/* <Patient_detail /> */}
-      {/* <BasicTabs /> */}
-      {/* <Notifications /> */}
-      {/* <MyProfile/> */}
-      {/* <MyTeam /> */}
-    </div>
+    
+    <Routes>
+      <Route path="/" element={<PageWithNavbar />}>
+          <Route index element={<Navigate to="login" replace />} />
+
+          <Route path='login' element={< LoginPage />}/>
+          <Route path='notification' element={< Notifications />}/>
+          <Route path='myteam' element={< MyTeam />}/>
+          <Route path='myprofile' element={< MyProfile />}/>
+          <Route path='patientinfo' element={< BasicTabs />}/>
+          <Route path='patientdetails' element={< PatientListGrid />}/>
+
+      </Route>
+
+
+    </Routes>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { AppContextProvider } from './AppContextProvider'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppContextProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AppContextProvider>
   </React.StrictMode>,
 )
