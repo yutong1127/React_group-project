9babba62b16c1e931ed08caffc70cbc1b1f6e194
yutong1127 <98676913+yutong1127@users.noreply.github.com>
2023-04-14 16:12:48 +1200

    patientdetails

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react';
-import './App.css';
-
+import React from "react";
+import Patient_detail from "./Patient_detail";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
-      <p>testing</p>
+      <Patient_detail />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { StyledEngineProvider } from "@mui/material/styles";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <StyledEngineProvider injectFirst>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StyledEngineProvider>
+  </React.StrictMode>
+);
