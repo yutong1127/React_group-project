3634e324469eda6031042a91a1894fe4b72ceba9
JingyiEureka <40253730+JingyiEureka@users.noreply.github.com>
2023-04-23 23:54:20 +1200

    added update upderprofile connectivity

--- backend/src/routes/api/user-Profile.js
+++ backend/src/routes/api/user-Profile.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
-    getUserById
+    getUserById,
+    updateUserProfile
 } from '../../dao/user-dao.js'
 
 const HTTP_CREATED = 201;
@@ -11,8 +12,7 @@ const router = express.Router();
 
 router.get('/:userId', async(req,res)=>{
     const { userId } = req.params;
-    console.log("user id is");
-    console.log(userId);
+
 
     const user =await getUserById(userId);
 
@@ -23,6 +23,18 @@ router.get('/:userId', async(req,res)=>{
 
 });
 
+router.put('/:userId', async(req,res)=>{
+    const { userId } = req.params;
+  
+    const userProfile = req.body;
+
+    userProfile._id = userId;
+
+    const succcess = await updateUserProfile(userProfile);
+
+    res.sendStatus(succcess ? HTTP_NO_CONTENT : HTTP_NOT_FOUND);
+
+});
 
 
 
