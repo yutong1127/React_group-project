60e7b371bb103f3acac1b5a50fcc0ccff134ec75
chanjant <chanjant@gmail.com>
2023-04-28 20:00:50 +1200

    progress & task

--- frontend/src/pages/PatientListPage/OverviewPage/Overview.jsx
+++ frontend/src/pages/PatientListPage/OverviewPage/Overview.jsx
@@ -1,10 +1,34 @@
 import PatientCard from './PatientCard'
 import { AppContext } from '../../../utils/AppContextProvider'
-import { useContext } from 'react'
+import { useContext,useState, React, useEffect } from 'react'
+import axios from 'axios';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? '';
 
 export default function Overview() {
 
-    const { patients } = useContext(AppContext)
+    //const { patients } = useContext(AppContext)
+
+    //Jant: Retrieve all patients or by team?
+    const [patients, setPatients] = useState([]);
+    // temporary for testing
+    useEffect(() => {
+        async function getPatients() {
+            const p = [];
+            const {data}  = await axios.get(`${API_BASE_URL}/api/team/1/patient_list`);
+            for (const d of data) {
+                const patient = {
+                    name: `${d.fname} ${d.lname}`,
+                    location: d.location,
+                    identifier: d._id
+                }
+                p.push(patient)
+            }
+            setPatients(p);
+        }
+        getPatients();
+        console.log(patients);
+    }, []);
 
     return (
         <div>
--- frontend/src/pages/PatientListPage/OverviewPage/PatientTasks.jsx
+++ frontend/src/pages/PatientListPage/OverviewPage/PatientTasks.jsx
@@ -4,16 +4,36 @@ import TasksRad from "./TasksRad"
 import { Button, Grid } from '@mui/material'
 import styles from './PatientTasks.module.css'
 // import Grid  from '@mui/material/Unstable_Grid item';
+import { useContext,useState, React, useEffect } from 'react'
+import axios from 'axios';
 
-export default function PatientTasks() {
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? '';
+
+export default function PatientTasks(props) {
+    const [taskBlood, setTaskBlood] = useState(false);
+    const [taskRad, setTaskRad] = useState(false);
+    const [taskHisto, setTaskHisto] = useState(false);
+
+    useEffect(() => {
+        async function getTasks() {
+            const { data }  = await axios.get(`${API_BASE_URL}/api/task/patient/${props.patient.identifier}`);
+            if(data == 'Blood Test') {
+                setTaskBlood(true);
+            }
+            if(data == 'Radiology') {
+                setTaskRad(true);
+            }
+        }
+        getTasks();
+    }, []);
 
     return (
         <Grid item container spacing={2} className={styles.patientTasks}>
             <Grid item xs={6}>
-                <TasksBlood />
+                {taskBlood && <TasksBlood />}
             </Grid >
             <Grid item xs={6}>
-                <TasksRad />
+                {taskRad && <TasksRad />}
             </Grid >
             <Grid item xs={6}>
                 <TasksHisto />
