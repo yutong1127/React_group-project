16ceecc7b411606f3cb7aefaf9eb7871b09701ee
Zhiyan Hu <zhu531@aucklanduni.ac.nz>
2023-05-03 17:48:54 +1200

    fix refresh and close tab maintain login issue

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -3,6 +3,11 @@ export function authenticate(req, res, next) {
     console.log("authenticate being called")
     req.loggedIn = true;
     req.user = req.session.user;
+    // if (req.user.isAdmin) {
+    //   req.isAdmin = true;
+    // } else {
+    //   req.isAdmin = false;
+    // }
   } else {
     req.loggedIn = false;
     res.status(401).json({ message: 'Unauthorized access' });
--- frontend/src/utils/AppContextProvider.jsx
+++ frontend/src/utils/AppContextProvider.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import useGet from "../hooks/useGet";
 import useGetUser from "../hooks/useGetUser";
+import { logoutUser } from "../api/user";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "";
 
@@ -18,6 +19,7 @@ export const AppContext = React.createContext({
 });
 
 export function AppContextProvider({ children }) {
+
   const [loggedIn, setLoggedIn] = useState(false);
   const [loggedInUser, setLoggedInUser] = useState(null);
 
@@ -30,6 +32,15 @@ export function AppContextProvider({ children }) {
     }
   }, [loggedInUser]);
 
+  useEffect(() => {
+    const savedUser = sessionStorage.getItem('loggedInUser');
+
+    if (savedUser) {
+      setLoggedInUser(JSON.parse(savedUser));
+      setLoggedIn(true);
+    }
+  }, []);
+  
 
   const {
     data: notification,
